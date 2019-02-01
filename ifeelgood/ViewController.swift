0112ab3c6d95fa0754e88ce062f2858aca2d1aa6
Ethan John <ejjonny@gmail.com>
2019-01-31 23:02:48 -0700

    Initial Commit

---
+++ ifeelgood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ifeelgood
+//
+//  Created by Ethan John on 1/31/19.
+//  Copyright © 2019 ya boy E. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
